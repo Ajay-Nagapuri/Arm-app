53c35310fea81b09cc99802a4176948d55e55a50
Ajay-Nagapuri <ajaynagapuri50@gmail.com>
2025-02-03 23:07:51 +0530

    second commit

--- test.py
+++ test.py
@@ -33,4 +33,7 @@ elif choice == '3':
 elif choice == '4':
     print("Result:", divide(num1, num2))
 else:
-    print("Invalid input this was a uncomitrd!")
+    print("Invalid input this was a uncomitrd5!")
+
+    print ("hello world")
+    
